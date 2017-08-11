4366f5bccc48823f62a4685c946a48ee16a7e446
Oldperl <44996956+oldperl@users.noreply.github.com>
2017-08-11 12:38:40 +0000

    data files for release added (testing)

--- setup/data/base/base_misc1.sql
+++
@@ -1,1 +0,0 @@
-UPDATE !PREFIX!_art_lang SET published=lastmodified, publishedby=modifiedby WHERE online=1 and published="0000-00-00 00:00:00";
--- setup/data/base/base_nav_main1.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM !PREFIX!_nav_main WHERE idnavm < 10000;
-INSERT INTO !PREFIX!_nav_main VALUES('1', 'content', 'navigation/content/main');
-INSERT INTO !PREFIX!_nav_main VALUES('2', 'style', 'navigation/style/main');
-INSERT INTO !PREFIX!_nav_main VALUES('4', 'statistic', 'navigation/statistic/main');
-INSERT INTO !PREFIX!_nav_main VALUES('5', 'administration', 'navigation/administration/main');
-INSERT INTO !PREFIX!_nav_main VALUES('3', 'extra', 'navigation/extra/main');
