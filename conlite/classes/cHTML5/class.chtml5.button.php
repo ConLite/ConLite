12f2b5795163d849fc0c31f591995b74404f2b3a
o.pinke <o.pinke@conlite.org>
2020-07-07 13:14:29 +0200

    clean up; git admin; add phpdoc config; fix setup bug

--- conlite/classes/cHTML5/class.chtml5.button.php
+++ conlite/classes/cHTML5/class.chtml5.button.php
@@ -6,17 +6,13 @@
  * Description:
  *  cHTML Meta
  * 
- * @package Core
- * @subpackage cHTML
- * @version $Rev$
+ * @package ConLite\Core\cHTML5
  * @since 2.0
  * @author Ortwin Pinke <o.pinke@conlite.org>
  * @copyright (c) 2015, conlite.org
  * @license http://www.gnu.de/documents/gpl.en.html GPL v3 (english version)
  * @license http://www.gnu.de/documents/gpl.de.html GPL v3 (deutsche Version)
  * @link http://www.conlite.org ConLite.org
- * 
- * $Id$
  */
 
 // security check
