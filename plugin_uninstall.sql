1de173c80a85f3fce9eedf8230178abe5bfced58
o.pinke <o.pinke@conlite.org>
2021-08-17 19:07:35 +0200

    Squashed 'conlite/plugins/cl-content-allocation/' content from commit 9fab054
    git-subtree-dir: conlite/plugins/cl-content-allocation
    git-subtree-split: 9fab0549299f5a17d8b242f3aaa13cdfe83c90d5

---
+++ plugin_uninstall.sql
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  oldperl
+ * Created: 06.05.2021
+ */
+
+DROP TABLE IF EXISTS !PREFIX!ca_alloc;
+DROP TABLE IF EXISTS !PREFIX!ca_alloc_con;
+DROP TABLE IF EXISTS !PREFIX!ca_lang;
