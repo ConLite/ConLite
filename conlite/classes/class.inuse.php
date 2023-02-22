7c69afa32641fe5e25f6e95f36245b3958e8d72b
o.pinke <o.pinke@conlite.org>
2023-02-22 20:10:59 +0100

    temporary fix for inuse message if a module is used for body tag in layout

--- conlite/classes/class.inuse.php
+++ conlite/classes/class.inuse.php
@@ -255,7 +255,7 @@ class InUseCollection extends ItemCollection
                 }
 
                 if (!is_object($notification)) {
-                    $notification = new Contenido_Notification;
+                    $notification = new Contenido_Notification();
                 }
 
                 $noti = $notification->messageBox("warning", $message.$override, 0);
