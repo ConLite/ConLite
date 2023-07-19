ccff65a4e955e34f07c3ec6d427fe910642496d0
o.pinke <o.pinke@conlite.org>
2023-07-19 18:43:26 +0200

    optimize rector by using conlite autoloader

--- rector.php
+++ rector.php
@@ -6,6 +6,16 @@ use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
+    /*
+    $rectorConfig->autoloadPaths([
+        __DIR__ . '/conlite',
+        __DIR__ . '/conlib',
+        __DIR__ . '/setup',
+    ]);
+    */
+    $rectorConfig->bootstrapFiles([
+        __DIR__ . '/rector_cl_autoload.php',
+    ]);
     $rectorConfig->parallel();
     $rectorConfig->paths([
         __DIR__.'/conlite',
