95444fed6a1e1ea93cfd819e370d3a856a39cc5e
o.pinke <o.pinke@conlite.org>
2024-04-22 15:05:07 +0200

    new file doc block

--- conlite/includes/functions.includePluginConf.php
+++ conlite/includes/functions.includePluginConf.php
@@ -2,18 +2,15 @@
 
 /**
  *
- * @package	Includes
- * @subpackage  Plugins
- * @version	$Rev$
+ * @package	ConLite\Includes\Plugin\Config
  * @since 2.0
  * @author Ortwin Pinke <o.pinke@conlite.org>
  * @copyright (c) 2015, conlite.org
  * @license http://www.gnu.de/documents/gpl.en.html GPL v3 (english version)
  * @license http://www.gnu.de/documents/gpl.de.html GPL v3 (deutsche Version)
  * @link http://www.conlite.org ConLite.org
- *
- *   $Id$:
  */
+
 defined('CON_FRAMEWORK') || die('Illegal call: Missing framework initialization - request aborted.');
 
 $bDebug = FALSE;
