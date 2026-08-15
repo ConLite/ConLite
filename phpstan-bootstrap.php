5ed6b57ddc4cc735bb19cad0a47668a40987ea82
Oldperl <ortwin.pinke@php-backoffice.de>
2026-08-15 14:30:26 +0200

    edit rector, add phpstan bootstrap

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,28 @@
+<?php
+if (!defined("CON_FRAMEWORK")) {
+    define("CON_FRAMEWORK", true);
+}
+
+if (!defined('CL_VERSION')) {
+
+    define('CL_VERSION', '3.1.0');
+
+}
+
+$sPathCfgDir = dirname(__FILE__) . '/data/config/production/';
+
+include_once($sPathCfgDir . 'config.php');
+include_once($sPathCfgDir . 'config.path.php');
+include_once($sPathCfgDir . 'config.misc.php');
+include_once($sPathCfgDir . 'config.colors.php');
+include_once($sPathCfgDir . 'config.path.php');
+include_once($sPathCfgDir . 'config.templates.php');
+
+$cfg['path']['config'] = $sPathCfgDir;
+
+// Various base API functions
+require_once($cfg['path']['conlite'] . $cfg['path']['includes'] . '/api/functions.api.general.php');
+
+// Initialization of autoloader
+include_once($cfg['path']['conlite'] . $cfg['path']['classes'] . 'class.autoload.php');
+cAutoload::initialize($cfg);
